8b1ba8b193e0f8f6d0da325f292297cac8facb83
Junhyuk Noh <junhyuk.noh@kurlycorp.com>
2021-08-09 03:20:29 +0900

    상세 조회를 구현 하라

--- src/main/java/com/spinrgmvc/board/controller/BoardController.java
+++ src/main/java/com/spinrgmvc/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.spinrgmvc.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +17,12 @@ public class BoardController {
     public String list(Model model) { // dispatcherServlet 이 모델을 생성 해 중
         model.addAttribute("boardList", boardService.list());
         return "board/list"; // View 힌트, dispatcherServlet 이 사용자에게 보여줄 뷰를 선정
+    }
+
+    @RequestMapping(value = "/board/read/{seq}")
+    public String read(Model model, @PathVariable int seq) {
+        model.addAttribute("boardVO", boardService.read(seq));
+        return "board/read";
     }
 
     @RequestMapping(value = "/board/write")
