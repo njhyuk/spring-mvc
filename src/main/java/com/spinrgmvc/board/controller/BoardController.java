fa836b41a448eb42b2723a3d1c0eb3d29f7354b0
Junhyuk Noh <junhyuk.noh@kurlycorp.com>
2021-08-09 09:47:53 +0900

    글 수정을 구현하라

--- src/main/java/com/spinrgmvc/board/controller/BoardController.java
+++ src/main/java/com/spinrgmvc/board/controller/BoardController.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
 @Controller
+@SessionAttributes("boardVO")
 public class BoardController {
     @Autowired
     private BoardService boardService;
@@ -40,13 +40,40 @@ public class BoardController {
      * bindingResult : boardVO 바인딩 할때 오류가 발생하는 경우 오류 내용을 저장함
      */
     @RequestMapping(value = "/board/write", method = RequestMethod.POST)
-    public String write(@Valid BoardVO boardVO, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
+    public String write(@Valid BoardVO boardVO, BindingResult result) {
+        if (result.hasErrors()) {
             return "/board/write";
         } else {
             boardService.write(boardVO);
             return "redirect:/board/write";
         }
+    }
 
+    @RequestMapping(value = "/board/edit/{seq}", method = RequestMethod.GET)
+    public String edit(@PathVariable int seq, Model model) {
+        model.addAttribute("boardVO", boardService.read(seq)); // 세션에 자동 등록됨
+        return "/board/edit";
+    }
+
+    @RequestMapping(value = "/board/edit/{seq}", method = RequestMethod.POST)
+    public String edit(
+            @Valid @ModelAttribute BoardVO boardVO, // 세션에서 boardVO 바인딩 됨 (HttpServletRequest)
+            int pwd,
+            SessionStatus sessionStatus,
+            BindingResult result,
+            Model model
+    ) {
+        if (result.hasErrors()) {
+            return "/board/edit";
+        } else {
+            if (boardVO.getPassword() == pwd) {
+                boardService.edit(boardVO);
+                sessionStatus.setComplete(); // 세션에서 해제
+                return "redirect:/board/list";
+            }
+        }
+
+        model.addAttribute("msg", "비밀번호가 일치하지 않습니다.");
+        return "/board/edit";
     }
 }
